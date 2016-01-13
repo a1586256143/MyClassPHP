bf868b5a717e602be9c0387e6a4072717ff89655
colinadmin <1586256143@qq.com>
2016-01-13 23:00:19 +0800

    本次更新 1.权限类初始化

---
+++ MyClass/libs/Auth.class.php
@@ -0,0 +1,24 @@
+<?php
+/*
+	Author : Colin,
+	Creation time : 2016/1/13 22:43
+	FileType :权限类
+	FileName :Auth.class.php
+*/
+namespace MyClass\libs;
+class Auth{
+	protected $db;			//数据库句柄
+	protected $config;		//配置信息
+
+	public function __construct(){
+
+	}
+
+	/**
+	 * @return [type]
+	 * @author Colin <15070091894@163.com>
+	 */
+	public function check(){
+
+	}
+}

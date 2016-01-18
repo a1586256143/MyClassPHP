92fa5cad2510747d0c6b1c8abd6b146781506abc
colinadmin <1586256143@qq.com>
2016-01-18 20:55:19 +0800

    本次更新 1.更新默认初始化时的文件

--- MyClass/tpl/index.php
+++ MyClass/tpl/index.php
@@ -1,6 +1,8 @@
 <?php
+namespace Application\Controller;
+use MyClass\libs\Controller;
 //本类为系统生成的类，供测试使用
-class IndexController extends \MyClass\libs\Controller{
+class IndexController extends Controller{
 	public function index(){
 		echo "Welcome Select MyclassPHP";
 	}

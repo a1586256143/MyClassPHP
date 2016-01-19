ba7ddb78187eee3d085a53fbf286139765af2515
colinadmin <1586256143@qq.com>
2016-01-19 08:29:16 +0800

    本次更新 1.优化tpl文件夹

--- MyClass/tpl/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Application\Controller;
-use MyClass\libs\Controller;
-//本类为系统生成的类，供测试使用
-class IndexController extends Controller{
-	public function index(){
-		echo "Welcome Select MyclassPHP";
-	}
-}

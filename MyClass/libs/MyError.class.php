3ee9a1251f8b330bb6e038d9690ec8b3666524f1
Colin <1586256143@qq.com>
2016-03-23 01:59:56 -0400

    no commit message

--- MyClass/libs/MyError.class.php
+++ MyClass/libs/MyError.class.php
@@ -19,21 +19,6 @@ class MyError extends \Exception{
     }
 
     /**
-     * info初始化
-     */
-    protected static function info_initialize($code , $message , $file , $line , $detail){
-        header('Content-type:text/html;charset="utf-8"');
-        self::$info = "<div style='width:85%;height:100%;margin:0 auto;font-family:微软雅黑'>";
-        self::$info .= "<ul style='list-style:none;width:100%;height:100%;'>";
-        self::$info .= "<li style='height:40px;line-height:40px;font-size:20px;color:#333;word-break: break-all;'>Error级别：" . $code . "</li>";
-        self::$info .= "<li style='line-height:40px;font-size:20px;color:#333;word-break: break-all;'>Error信息：<font color='red' style='word-break: break-all;'>" . $message . "</font></li>";
-        self::$info .= "<li style='height:40px;line-height:40px;font-size:20px;color:#333;word-break: break-all;'>Error文件位置：" . $file . "</li>";
-        self::$info .= "<li style='height:40px;line-height:40px;font-size:20px;color:#333;word-break: break-all;'>Error行数：" . $line . "</li>";
-       // self::$info .= "<li style='height:40px;line-height:40px;font-size:20px;color:#333;word-break: break-all;'>Error详细：" . $detail . "</li>";
-        self::$info .= "</ul></div>";
-    }
-    
-    /**
      * 显示错误消息
      * @author Colin <15070091894@163.com>
      */
@@ -67,7 +52,7 @@ class MyError extends \Exception{
      */
     public static function shutdown_function(){
 		$e = error_get_last();
-        self::customError($e['type'] , $e['message'] , $e['file'] , $e['line'] , $e['traceasstring']);
+        self::customError($e['type'] , $e['message'] , $e['file'] , $e['line'] , null);
     }
 
     /**
@@ -88,6 +73,34 @@ class MyError extends \Exception{
      */
     protected static function set_error_show(){
         ini_set('display_errors', 'Off');
+    }
+
+    /**
+     * info初始化
+     * @param $code 错误等级
+     * @param $message 错误信息
+     * @param $file 错误文件
+     * @param $line 错误行数
+     * @param $detail 错误流程详情
+     * @author Colin <15070091894@163.com>
+     */
+    protected static function info_initialize($code , $message , $file , $line , $detail){
+        header('Content-type:text/html;charset="utf-8"');
+        self::$info = "<div style='width:85%;height:100%;margin:0 auto;font-family:微软雅黑'>";
+        self::$info .= "<ul style='list-style:none;width:100%;height:100%;'>";
+        self::$info .= "<li style='height:40px;line-height:40px;font-size:20px;color:#333;word-break: break-all;'>错误级别：" . $code . "</li>";
+        self::$info .= "<li style='line-height:40px;font-size:20px;color:#333;word-break: break-all;'>错误信息：<font color='red' style='word-break: break-all;'>" . $message . "</font></li>";
+        self::$info .= "<li style='height:40px;line-height:40px;font-size:20px;color:#333;word-break: break-all;'>错误文件：" . $file . "</li>";
+        self::$info .= "<li style='height:40px;line-height:40px;font-size:20px;color:#333;word-break: break-all;'>错误行数：" . $line . "</li>";
+        self::$info .= "<li style='height:40px;line-height:40px;font-size:20px;color:#333;word-break: break-all;'>";
+        $string = array_filter(explode("#" , $detail));
+        if(is_array($string)){
+            foreach ($string as $key => $value) {
+                self::$info .= '#'. $value . '<br>';
+            }
+        }
+        self::$info .= "</li>";
+        self::$info .= "</ul></div>";
     }
 }
 ?>

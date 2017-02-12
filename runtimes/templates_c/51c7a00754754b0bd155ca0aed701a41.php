2ba33defc16997f049f7b2226cd17b6917c5a36e
mac <1586256143@qq.com>
2017-02-12 12:42:55 +0800

    删除缓存文件

--- runtimes/templates_c/51c7a00754754b0bd155ca0aed701a41.php
+++
@@ -1,3 +0,0 @@
-
-<br>
-index.html
--- runtimes/templates_c/6a992d5529f459a44fee58c733255e86index.php
+++
@@ -1,4 +0,0 @@
-<?php echo dump(1111); ?>
-<?php echo __PUBLIC__; ?>  <br> 
-<?php echo __URL__; ?>	<br>
-<img src="<?php echo __PUBLIC__; ?>/images/img.jpg" alt="">

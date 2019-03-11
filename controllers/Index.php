ebdc2655b1aadd9c3a957aab7a24b74848171c19
Colin <1457877626@qq.com>
2019-03-11 13:38:57 +0800

    版本退回

--- controllers/Index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-namespace controllers;
-use system\Base;
-class Index extends Base{
-	public function index(){
-		return 'Welcome to use MyClassPHP';
-	}
-}
-?>
--- system/Templates/extendTemplate.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace system\Templates;
-//扩展模板
-interface extendTemplate{
-	//解析模板
-	public function parse($tpl);
-}

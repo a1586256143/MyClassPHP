6e3489d2e446e0d59513556df20a3838825ddf82
Colin <1586256143@qq.com>
2019-05-05 13:53:46 +0800

    增加command

--- make.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/php
-<?php
-if (PHP_SAPI !== 'cli') {
-    exit('Please run in CLI mode');
-}
-require_once 'index.php';
-$commands = new system\Command\Command($argv);
-echo $commands::exec($argv);
